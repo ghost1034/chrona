2dd4d7600502e206368f32bbb92fdd640106fa46
Ian Stewart <ianloyestewart@gmail.com>
2026-02-02 23:21:13 +0000

    Fix: import variable font from fontsource-variable

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/ibm-plex-sans'
+import '@fontsource/ibm-plex-mono/400.css'
 import { App } from './App'
 import './styles.css'
 
